f4d835eaa5ffb8838548f57c3b7224d445ceea8c
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2026-02-09 21:18:35 +0700

    Fix Netlify redirects file

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
-import { createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/home";
 import Detail from "./pages/detail";
 import NotFound from "./pages/notfound";
 import Layout from "./components/layout";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     element: <Layout />,
 
