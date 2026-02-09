341efb8eb1f054f121623281464bdafbf87246a1
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2026-02-09 21:41:59 +0700

    Add netlify.toml for functions

--- netlify/functions/coincap.js
+++ netlify/functions/coincap.js
@@ -1,7 +1,6 @@
 export async function handler() {
-  const res = await fetch("https://api.coincap.io/v2/assets?limit=10");
-
-  const data = await res.json();
+  const response = await fetch("https://api.coincap.io/v2/assets?limit=10");
+  const data = await response.json();
 
   return {
     statusCode: 200,
